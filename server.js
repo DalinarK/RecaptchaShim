81db4e4b7fe78ce618dba6b1c5a8e8291602652f
Dustin Dinh <djdinh@gmail.com>
2016-06-10 16:56:53 -0700

    Removed hardcoded URL and proof of concept code

--- server.js
+++ server.js
@@ -9,24 +9,15 @@ var request = require('request');
 // used to write to file system
 var fs = require('fs');
 
-//hardcoded client name into url for testing
-var bitServerCaptchaReset = 'http://localhost:7990/rest/api/1.0/admin/users/captcha?name=client';
 //url for REST Call
 var url = 'http://localhost:7990/rest/api/1.0/admin/users/captcha?name=';
 var adminName = 'overlord';
 var adminPass = '96rNAowc4Zet';
+//where to save the logfile
 var rootFolder = '/Computer\ Science\ OSU/BitBucket\ Shim/LogFile.txt';
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-
-app.get('/resetCredentials', function(req, res) {
-	console.log("I received a delete request!!!");
-	request.delete(bitServerCaptchaReset, function optionalCallback(err, httpResponse, body) {res.json(httpResponse)}).auth(adminName, adminPass, true)
-	{
-		console.log("sent delete request");
-	};
-});
 
 // Receives the POST request then passes the client to the person.
 app.post('/resetCredentials', function(req, res) {
